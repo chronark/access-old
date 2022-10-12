59bef02626ab60a71917e688328758a634155223
Andreas Thomas <dev@chronark.com>
2022-10-12 23:21:11 +0200

    fix: build for cjs

--- tsup.config.js
+++ tsup.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  dts: true
+    entry: ['src/index.ts'],
+    format: ["cjs", "esm"],
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    bundle: true,
+    dts: true,
+
 })
